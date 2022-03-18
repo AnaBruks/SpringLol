f4a61c915ede39a68d3626cec55fe47e1a194c37
AnaBruks <vasin.rrr@gmail.com>
2022-03-18 19:15:30 +0200

    1st commit: IoC

---
+++ src/main/java/org/spr/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package org.spr;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Classical Music";
+    }
+}
---
+++ src/main/java/org/spr/RockMusic.java
@@ -0,0 +1,8 @@
+package org.spr;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Rock Music";
+    }
+}
